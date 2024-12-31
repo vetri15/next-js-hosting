c51606c056d51d9664103603fcd94fc86d522a3e
vetri15 <64657217+vetri15@users.noreply.github.com>
2024-12-31 18:06:00 +0530

    test 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
-  output: "export",
-  reactStrictMode: true,
-  basePath: "/next-js-hosting",
-  images: {
-    unoptimized: false,
-  }
+  // distDir: 'build',
+  output: "export"
+  // reactStrictMode: true,
+  // basePath: "/next-js-hosting",
+  // images: {
+  //   unoptimized: false,
+  // }
 };
 
 export default nextConfig;
